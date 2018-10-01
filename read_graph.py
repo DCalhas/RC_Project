eb31e1a19ee5b6a9c0c09b29515d44ef65491598
DCalhas <davidcalhas@hotmail.com>
2018-10-01 16:48:45 +0100

    uni

--- read_graph.py
+++ read_graph.py
@@ -1,9 +1,18 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 
-nx.read_graphml('airlines.graphml')
+#read graph
+G = nx.read_graphml('airlines.graphml')
 
 
 
+print("Average Path Length: ", nx.average_shortest_path_length(G))
 
+
+print(G['1'])
+nx.draw(G)
+
+
+plt.show()
