ea495e1b025d0dc0867fc7a2fb307650f596e2be
joao <jvalente96@gmail.com>
2018-10-09 02:04:45 +0100

    code clean, made plots prettier, adding more cool plots soon

--- read_graph.py
+++ read_graph.py
@@ -4,27 +4,28 @@ import collections
 import numpy as np
 
 
-#read graph
+# Reat the graph
 G = nx.read_graphml('airlines.graphml')
 
+# https://networkx.github.io/documentation/networkx-1.10/reference/algorithms.clustering.html
+# https://networkx.github.io/documentation/networkx-1.10/reference/algorithms.centrality.html
 
-
-#https://networkx.github.io/documentation/networkx-1.10/reference/algorithms.clustering.html
-#https://networkx.github.io/documentation/networkx-1.10/reference/algorithms.centrality.html
-
-
+# How big the medium trip is
 print("Average Path Length: ", nx.average_shortest_path_length(G))
 
+# How popular are the airports that connect to the popular/unpopular airports
 print("Average Degree Connectivity (mean k): ", nx.average_degree_connectivity(G))
 
-#print("Average Clustering: ", nx.average_clustering(G))
+# Average times that airports B<->C given that A<->C and A<->B
+# print("Average Clustering: ", nx.average_clustering(G))
 
+# Gives us how close each airport is to the others. Can compute the airport closest to every other in the world
 print("Closeness Centrality: ", nx.closeness_centrality(G))
 
-#TODO degree distribution histogram 
+# The node that appears more times in the shortest paths between two other nodes
+print("Closeness Centrality: ", nx.betweenness_centrality(G))
 
-
-
+#TODO degree distribution histogram
 
 
 
@@ -32,50 +33,46 @@ print("Closeness Centrality: ", nx.closeness_centrality(G))
 #Computing degree of the nodes and the respective number of nodes that have that degree
 ###################################################################################
 
+# Get list with degrees of each node
 degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
 
+print("###################################################################################")
 
+# Dic with how much time each degree appeared
 degreeCount = collections.Counter(degree_sequence)
 
+# Separate dic into two separated tuples
 deg, cnt = zip(*degreeCount.items())
-
-
 cnt = list(cnt)
 
 
-#normalize cnt
+# Normalize cnt getting %
 total_sum = sum(cnt)
 for i in range(len(cnt)):
 	cnt[i] = cnt[i]/total_sum
 
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-
-
-
-
 
 ###################################################################################
-#Plotting the power law in a log log scale ---- Non cummulative plot
+# Plotting the power law in a log log scale ---- Non cummulative plot
 ###################################################################################
 
 ax = plt.subplot(211)
 
 plt.loglog(deg, cnt, 'o')
 
+plt.title("Degree Distribution (log/log)")
+plt.ylabel("log₁₀ P(k)")
+plt.xlabel("log₁₀ k")
 
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
+plt.show()
 
 
 ###################################################################################
-#Plotting the power law in a log log scale ---- Cummulative plot
+# Plotting the power law in a log log scale ---- Cummulative plot
 ###################################################################################
 
 
-#build cummulative vector
+# Build cummulative vector
 cnt_cdf = []
 for i in range(len(cnt)):
 	cnt_cdf += [sum(cnt[0:i+1])]
@@ -84,21 +81,11 @@ ax = plt.subplot(212)
 
 plt.loglog(deg, cnt_cdf, 'o')
 
+plt.title("Cumulative Degree Distribution (log/log)")
+plt.ylabel("log₁₀ P_cum(k)")
+plt.xlabel("log₁₀ k")
+
 plt.show()
-
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
 
 ###################################################################################
 #  Plot the degree distribution based on the deg and cnt lists
@@ -108,23 +95,8 @@ plt.show()
 fig, ax = plt.subplots()
 plt.bar(deg, cnt, width=0.5, color='b', tick_label=[d for d in deg])
 
-plt.title("Degree Distribution Histogram")
-plt.ylabel("Count")
-plt.xlabel("Degree")
-
-plt.show()
-
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-
-
-
-
-
-
-
-#nx.draw(G)
-
+plt.title("Degree Distribution")
+plt.ylabel("Probability P(k)")
+plt.xlabel("Degree k")
 
 plt.show()
